dd9a2105bbea0c040005aa6da8f3cb774d178d68
JNS <65025344+jinto4638@users.noreply.github.com>
2021-03-16 20:28:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -14,14 +14,19 @@ SHARE AND SUPPORT @FCfilmcornerfc 💘💝
 
     HELP_MSG = """<b>How to use the bot??</b>
    ⬇️⬇️⬇️⬇️⬇️
+👨‍🦯👨‍🦯👨‍🦯
 Contact on @TroJanzSupport📢
+
 <b> @FCfilmcornerfc</b>🎥
+
 <b>© @TroJanzHEX</b>"""
 
 
     ABOUT_MSG = """📝<b>My Name : FC™ AUTO FILTER BOT </b>
     
 🧬<b>Owner :</b> @JINTONS (forked from @TroJanZheX)
+
 ⚙️<b>Language :</b> <code>Python3</code>
+
 📂<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 """
