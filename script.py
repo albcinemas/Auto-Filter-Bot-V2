5dd44ac991d2a9c4ddc013ff0220ad52d2d6e49d
JNS <65025344+jinto4638@users.noreply.github.com>
2021-03-16 20:24:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,55 +13,15 @@ SHARE AND SUPPORT @FCfilmcornerfc 💘💝
 
 
     HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
+   ⬇️⬇️⬇️⬇️⬇️
+Contact on @TroJanzSupport📢
+<b> @FCfilmcornerfc</b>🎥
 <b>© @TroJanzHEX</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """📝<b>My Name : FC™ AUTO FILTER BOT </b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
-
+🧬<b>Owner :</b> @JINTONS (forked from @TroJanZheX)
+⚙️<b>Language :</b> <code>Python3</code>
+📂<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 """
