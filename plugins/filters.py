4b70bca0cf0c5ca33bd80482455d70bdf5a82f79
JNS <65025344+jinto4638@users.noreply.github.com>
2021-03-23 12:50:50 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -62,7 +62,7 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
             await message.reply_text(
-                f"<b> •⊹٭𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢٭⊹• @FCfilmcornerfc \n\n\n👇Here is the result of <u> {message.text}  </u>👇</b>",
+                f"<b>•⊹٭𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢٭⊹• @FCfilmcornerfc \n\n\n👇Here is the result of <u> {message.text}  </u>👇</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
             return
@@ -78,7 +78,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"<b> •⊹٭𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢٭⊹• @FCfilmcornerfc \n\n\n👇Here is the result of <u> {message.text}  </u> 👇</b>",
+                f"<b>•⊹٭𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢٭⊹• @FCfilmcornerfc \n\n\n👇Here is the result of <u> {message.text}  </u> 👇</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
